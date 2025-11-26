cd7f4d7cdb58c38084d55ba0e9616057c769a3e5
manishcoeptech-hub <manish.coeptech@gmail.com>
2025-11-27 02:07:06 +0530

    Add files via upload

---
+++ public/public/script.js
@@ -0,0 +1,31 @@
+function drawPuzzle(state) {
+  const puzzle = document.getElementById("puzzle");
+  puzzle.innerHTML = "";
+
+  state.forEach(n => {
+    const tile = document.createElement("div");
+    tile.className = "tile" + (n === 0 ? " blank" : "");
+    tile.innerText = n === 0 ? "" : n;
+    puzzle.appendChild(tile);
+  });
+}
+
+async function solvePuzzle() {
+  const start = document.getElementById("start").value.split(" ").map(Number);
+  const goal  = document.getElementById("goal").value.split(" ").map(Number);
+  const algorithm = document.getElementById("algorithm").value;
+
+  const result = await fetch("/solve", {
+    method: "POST",
+    headers: { "Content-Type": "application/json"},
+    body: JSON.stringify({ start, goal, algorithm })
+  });
+
+  const data = await result.json();
+  const steps = data.steps;
+
+  for (let i = 0; i < steps.length; i++) {
+    drawPuzzle(steps[i]);
+    await new Promise(r => setTimeout(r, 700));
+  }
+}
